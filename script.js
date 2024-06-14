360f52786dbcf353753d8382b8732d22ef9bf793
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-06-14 20:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function volume_sphere() {
+const form = document.getElementById('MyForm');
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let r = Number(form.radius.value);
+	let volume = (4 * Math.PI * r ** 3) / 3;
+	form.volume.value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
